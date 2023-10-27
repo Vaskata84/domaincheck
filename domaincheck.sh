87f10aa450e5590fdfe950c9d5c21fb8dce93248
Vasil Dobchev <vasil.dobchev@gmail.com>
2023-10-27 15:36:24 +0300

    Create domaincheck.sh

---
+++ domaincheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ANSI escape кодове за цветове
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+RESET="\033[0m"
+
+while IFS= read -r domain; do
+    if curl --connect-timeout 5 --silent --head --fail "$domain"; then
+        echo -e "${GREEN}$domain е достъпен${RESET}"
+    else
+        echo -e "${RED}$domain не е достъпен${RESET}"
+    fi
+done < domains.txt
+
